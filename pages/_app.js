e629207756771ae5ba412ce1ed670e4e36892e5f
Chitraksh Maheshwari <68012660+chitrakshbotwala@users.noreply.github.com>
2023-07-10 20:41:33 +0530

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -45,7 +45,7 @@ export default function App({
             />
                      <Analytics />
             <Component {...pageProps} />
-                  <Component {...pageProps} />
+                  
   
      
   
