0369c8fd1e93afd881bc512fe803a48266ac94e2
Chitraksh Maheshwari <68012660+chitrakshbotwala@users.noreply.github.com>
2023-07-17 20:42:51 +0530

    Update leftBar.js

--- components/manga/leftBar.js
+++ components/manga/leftBar.js
@@ -80,7 +80,7 @@ export function LeftBar({ data, page, info, currentId, setSeekPage }) {
                             onClick={() => setSeekPage(x.index)}
                           >
                             <Image
-                              src={`https://img.Streamable.live/image-proxy?url=${encodeURIComponent(
+                              src={`https://img.moopa.live/image-proxy?url=${encodeURIComponent(
                                 x.url
                               )}&headers=${encodeURIComponent(
                                 JSON.stringify({ Referer: x.headers.Referer })
